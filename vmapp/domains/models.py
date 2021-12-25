3b45b1f4c137517f7aeb1eed0b323022163ad85a
CodeASAP.pl <95665514+codeasap-pl@users.noreply.github.com>
2021-12-25 02:49:19 +0100

    Tests: Domain model

--- vmapp/domains/models.py
+++ vmapp/domains/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from common.models import BaseModel
 
+from django.core.validators import MinLengthValidator
+
 
 class Domain(BaseModel):
     class Meta:
         db_table = "domains"
 
-    domain = models.CharField(null=False, unique=True,
+    domain = models.CharField(null=False, unique=True, blank=False,
+                              validators=[MinLengthValidator(1)],
                               max_length=255)  # RFC1035
     is_enabled = models.BooleanField(default=True)
 
